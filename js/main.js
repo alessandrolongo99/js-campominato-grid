af8ad5ea794f590df14550c270b8c07b2baab10d
alessandrolongo99 <longoalessandro153@gmail.com>
2022-06-23 15:52:32 +0200

    milestone

--- js/main.js
+++ js/main.js
@@ -4,16 +4,24 @@ const button = document.getElementById('my_button');
 
 function generateNewSquare() {
     const square = document.createElement('div');
-    square.classList.add('square');
+    square.classList.add('square', 'flex_container');
     return square;
 }
 
 button.addEventListener('click', function () {
+    document.getElementById('container').style.display = 'flex';
     for (let i = 0; i < 100; i++) {
         const newSquare = generateNewSquare();
 
-        newSquare.innerHTML = i;
+        newSquare.addEventListener('click', function () {
+            newSquare.classList.toggle('activated');
+
+            console.log(newSquare.innerHTML);
+        });
+
+        newSquare.innerHTML = i + 1;
 
         container.append(newSquare);
     }
 });
+
