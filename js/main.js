352a151dc13e0ae83292d51dd708b217442f2f0b
alessandrolongo99 <longoalessandro153@gmail.com>
2022-06-23 16:11:28 +0200

    inseriti livelli

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const container = document.getElementById('container');
 
 const button = document.getElementById('my_button');
 
+const selection = document.getElementById('selection');
+
 function generateNewSquare() {
     const square = document.createElement('div');
     square.classList.add('square', 'flex_container');
@@ -10,18 +12,55 @@ function generateNewSquare() {
 
 button.addEventListener('click', function () {
     document.getElementById('container').style.display = 'flex';
-    for (let i = 0; i < 100; i++) {
-        const newSquare = generateNewSquare();
 
-        newSquare.addEventListener('click', function () {
-            newSquare.classList.toggle('activated');
+    if (selection.value == 0) {
+        for (let i = 0; i < 49; i++) {
+            const newSquare = generateNewSquare();
+            newSquare.classList.add('size_1');
 
-            console.log(newSquare.innerHTML);
-        });
+            newSquare.addEventListener('click', function () {
+                newSquare.classList.toggle('activated');
 
-        newSquare.innerHTML = i + 1;
+                console.log(newSquare.innerHTML);
+            });
 
-        container.append(newSquare);
+            newSquare.innerHTML = i + 1;
+
+            container.append(newSquare);
+        }
+    }
+
+    if (selection.value == 1) {
+        for (let i = 0; i < 81; i++) {
+            const newSquare = generateNewSquare();
+            newSquare.classList.add('size_2');
+
+            newSquare.addEventListener('click', function () {
+                newSquare.classList.toggle('activated');
+
+                console.log(newSquare.innerHTML);
+            });
+
+            newSquare.innerHTML = i + 1;
+
+            container.append(newSquare);
+        }
+    }
+
+    if (selection.value == 2) {
+        for (let i = 0; i < 100; i++) {
+            const newSquare = generateNewSquare();
+            newSquare.classList.add('size_3');
+
+            newSquare.addEventListener('click', function () {
+                newSquare.classList.toggle('activated');
+
+                console.log(newSquare.innerHTML);
+            });
+
+            newSquare.innerHTML = i + 1;
+
+            container.append(newSquare);
+        }
     }
 });
-
